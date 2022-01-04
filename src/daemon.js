5e066bd149200c49485ad4f863b3d449dc37bb92
Tatiana Nikonova <taniushka_2007@mail.ru>
2022-01-04 22:59:30 +0300

    Initial

---
+++ src/daemon.js
@@ -0,0 +1,7 @@
+import MathChar from './mathChar';
+
+export default class Daemon extends MathChar {
+  constructor(name, type = 'Daemon') {
+    super(name, type);
+  }
+}
---
+++ src/magician.js
@@ -0,0 +1,7 @@
+import MathChar from './mathChar';
+
+export default class Magician extends MathChar {
+  constructor(name, type = 'Magician') {
+    super(name, type);
+  }
+}
